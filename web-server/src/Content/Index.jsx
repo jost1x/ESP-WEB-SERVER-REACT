641c6fae888c917eac590c9568df9ecc135edef8
Charlie Melchiori <jost1@live.com>
2023-07-09 23:59:12 -0400

    led button

--- web-server/src/Content/Index.jsx
+++ web-server/src/Content/Index.jsx
@@ -3,35 +3,55 @@ import axios from 'axios';
 
 export default function Index() {
 
-    const [data, setData] = useState(0);
+  const [data, setData] = useState(0);
 
 
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(`${import.meta.env.VITE_HOST_ESP}/sensor`);
-            setData(response.data);
-            console.log(response.data)
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        const interval = setInterval(() => {
-          fetchData();
-        }, 1000);
+  useEffect(() => {
+    fetchData()
 
-        return () => {
-          clearInterval(interval);
-        };
-      }, []);
+    const interval = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
 
-
-        return (<>
-            <h4>Temperatura CPU: { data ? data.SENSOR.TemperatureCPU : "..." }°C</h4>
-            <h4>ESTADO LED: { data ? data.SENSOR.GPIO2 : "..." }</h4>
-
-        </>)
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_HOST_ESP}/sensor`);
+      setData(response.data);
+      console.log(response.data)
+    } catch (error) {
+      console.error(error);
     }
+  };
+
+
+  const buttonClick = async () => {
+    await axios.post(`${import.meta.env.VITE_HOST_ESP}/sensor`, null, {
+      params: {
+        "GPIO2": "toggle"
+      }
+    });
+    fetchData();
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (<>
+    <h4>Temperatura CPU: {data ? data.SENSOR.TemperatureCPU : "..."}°C</h4>
+    <h4>ESTADO LED: {data ? data.SENSOR.GPIO2 : "..."}</h4>
+
+    <form onSubmit={handleSubmit}>
+      Control LED: <button onClick={buttonClick}>Toogle</button>
+    </form>
+
+  </>)
+}
